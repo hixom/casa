3e023e5f4d9b2aa795db16aac424de6678706bde
hixom <daniel_95_mz@hotmail.com>
2017-12-15 21:13:35 +0100

    he avanzado un poco

--- hundirLaFlota/src/hundirLaFlota/juego.java
+++ hundirLaFlota/src/hundirLaFlota/juego.java
@@ -17,19 +17,20 @@ public class juego {
 		Scanner sc=new Scanner(System.in);
 		
 		
-		arrayFichasDoble=valoresFichas(arrayFichasDoble.length,arrayFichasDoble[].length);
-		
+		arrayFichasDoble=valoresFichas(arrayFichasDoble.length,arrayFichasDoble[0].length);
+		arrayFichasDoble=ponerTodoNulo(arrayFichasDoble,arrayFichasDoble.length,arrayFichasDoble[0].length);
 	
-		arrayFichasTriple=valoresFichas(3);
-		
+		arrayFichasTriple=valoresFichas(arrayFichasTriple.length,arrayFichasTriple[0].length);
+		arrayFichasTriple=ponerTodoNulo(arrayFichasTriple,arrayFichasDoble.length,arrayFichasDoble[0].length);
 
-		arrayFichasTriple=valoresFichas(1);
+		arrayFichasCuadruple=valoresFichas(arrayFichasCuadruple.length,arrayFichasCuadruple[0].length);
+		arrayFichasCuadruple=ponerTodoNulo(arrayFichasCuadruple,arrayFichasDoble.length,arrayFichasDoble[0].length);
 	
 		terreno tablero= new terreno(10,10);
 		
 		for(int i = 0; i>4; i++) {
 			
-			tablero.InsertarFichas(valor, posicionArrayX, posicionArrayY)
+			tablero.InsertarFichas(valor, 10, 10);
 			
 		}
 		
@@ -43,52 +44,99 @@ public class juego {
 		
 		
 	}
-
+	public static int[][] ponerTodoNulo(int[][] arrayFichas, int repeticiones1, int repeticiones2) {
+		for (int i=0;i<repeticiones1;i++) {
+			for (int j=0;j<repeticiones1;j++) {
+				arrayFichas[i][j]=-1;
+			}
+		}
+		return arrayFichas;
+	}
 	
-	public static int[] pedirValores() {
-		int valorX;
-		int valorY;
-		int valorXFinal;
-		int valorYFinal;
-		int[] arrayValores= new int[4];
+	public static int[] valoresFichas(int nCoordenadas) {
+		final int ULTIMO = nCoordenadas;
+		final int PENULTIMO = nCoordenadas-1;
+		
+		int cantidadIntermediosX;
+		int cantidadIntermediosY;
+		int[] arrayValores= new int[nCoordenadas];
+		int diferenciaX;
+		int diferenciaY;
+		
 		
 		Scanner sc=new Scanner(System.in);
 		
 		do {
 			System.out.println("Vamos a elegir la ficha que solo ocupa una casilla, procedamos...");
-			System.out.println("introduce valor X");
-			valorX = sc.nextInt();
-			System.out.println("introduce valor y");
-			valorY=sc.nextInt();
-			System.out.println("introduce hasta que valor X");
-			valorXFinal = sc.nextInt();
-			System.out.println("introduce hasta que valor y");
-			valorYFinal =sc.nextInt();
-		}	while((valorX<0 || valorX>10 || valorY<0 || valorY>10) || (valorX != valorXFinal && valorY != valorYFinal ) );
-		arrayValores[0] = valorX;
-		arrayValores[1] = valorY;
-		arrayValores[2] = valorXFinal;
-		arrayValores[3] = valorYFinal;
+			for(int i=0; i<nCoordenadas;i++) {
+				switch(i){
+					case 0:	
+						System.out.println("introduce valor X por donde va a empezar la cardinalidad de la ficha");
+						arrayValores[i] = sc.nextInt();
+						break;
+					case 1:	
+						System.out.println("introduce valor Y por donde va a empezar la cardinalidad de la ficha");
+						arrayValores[i] = sc.nextInt();
+						break;
+					
+					case PENULTIMO:  
+						System.out.println("introduce valor X final de la cardinalidad de la ficha");
+						arrayValores[i] = sc.nextInt();
+						break;
+					case ULTIMO:
+						System.out.println("introduce valor Y final de la cardinalidad de la ficha");
+						arrayValores[i] = sc.nextInt();
+						break;
+							
+				}
+			
+			
+			}
+			
+			diferenciaX=Math.abs(arrayValores[0]-arrayValores[PENULTIMO]);
+			diferenciaY=Math.abs(arrayValores[1]-arrayValores[ULTIMO]);
+			cantidadIntermediosX= nCoordenadas - diferenciaX;
+			cantidadIntermediosY= nCoordenadas - diferenciaY;
+			
+/*			las lineas de abajo lo que hace es: en el while, en el primer paréntesis lo que hace es que se va a repetir mientras se
+			haya introducido un valor fuera de 0...10. 
+			El segundo, que se repita mientras no coincidan ni el valor de x 
+			con xFinal ni y con yFinal.
+			El siguiente paréntesis está compuesto de varios y para lo que sirven es para comparar la distancia que hay
+			entre la primera cordenada y la final y si es diferente de 0 o diferente de la cantidad que debería tener 
+			(si la ficha es de 3 coordenadas debería de haber una diferencia de 0 o 2) significa que o bien ha dejado 
+			espacio de más o de menos o ha puesto uno en oblicuo
+			
+			los if son para avisar que tipo de error es ya que hay bastantes tipos y puede ser muy desquiciante
+			estar equivocándose muchas veces sin saber un poco por qué
+			
+*/		
+			/*if(arrayValores[0]<0 || arrayValores[0]>10 || arrayValores[1]<0 || arrayValores[1]>10 || 
+			arrayValores[2]<0 || arrayValores[2]>10 || arrayValores[3]<0 || arrayValores[3]>10  ) {
+				System.out.println("has introducido algún valor que no corresponde al intervalo que hay entre 0 y 10");
+			}
+			if((diferenciaX != 0 || diferenciaX != cantidadIntermediosX) && 
+				(diferenciaY != 0 || diferenciaY != cantidadIntermediosY)) {
+				System.out.println("has ha dejado espacio de más o de menos entre el valor inicial y el valor final de"+ 
+				" alguna coordenada, o ha puesto alguna en oblicuo");
+			}
+		}while((arrayValores[0]<0 || arrayValores[0]>10 || arrayValores[1]<0 || arrayValores[1]>10 || 
+				arrayValores[2]<0 || arrayValores[2]>10 || arrayValores[3]<0 || arrayValores[3]>10  ) ||  
+				((diferenciaX != 0 || diferenciaX != cantidadIntermediosX) && 
+				(diferenciaY != 0 || diferenciaY != cantidadIntermediosY)));
+	
 		
 		return arrayValores;
 	}
-	public static int[][] valoresFichas(int nRepeticiones, int nCoordenadas) {
-		 int valorX;
-		 int valorY; 
-		 int valorXFinal; 
-		 int valorYFinal;
-		// int[] arrayValores=new int[4];
-		Scanner sc=new Scanner(System.in);
+	/*public static int[][] valoresFichas(int nRepeticiones, int nCoordenadas) {
 		
-		nRepeticiones -= 1;
+		int[][] arrayFichas=new int[nRepeticiones][nCoordenadas];
 		
-		int[][] arrayFichas=new int[nRepeticiones][];
-		
-		for (int i=nRepeticiones; i>0; i--) {
-		
-			arrayFichas[i]=new int[];
-		//crea un objeto en cada posicion del array y los valores de este los pasamos con una funcion-array
-			Ficha arrayFichas[i][] =  new Ficha( pedirValores() );
+		for (int i=0; i<nRepeticiones; i--) {
+			do {
+				Ficha arrayFichas[i][] =  new Ficha( pedirValores(nCoordenadas) );
+			}while (arrayFichas[i][])
+			
 			
 			
 		}
